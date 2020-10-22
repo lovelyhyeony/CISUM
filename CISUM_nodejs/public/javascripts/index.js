3a991beaaf200ef936034d8c216f0e522fa36bbf
lovelyhyeony <dmlgus1222@naver.com>
2020-10-22 16:40:55 +0900

    1022

--- CISUM_nodejs/public/javascripts/index.js
+++ CISUM_nodejs/public/javascripts/index.js
@@ -1,19 +1,16 @@
 $(function () {
+  $("div#login-modal").css("display", "none");
   $("#login_a").click(function () {
     $.ajax({
       url: "/cisum/login",
       type: "GET",
       success: function (result) {
-        $("section#login_modal").html(result);
+        $("div#login-modal").html(result);
+        $("div#login-modal").css("display", "block");
       },
       error: function (error) {
         alert("서버 통신 오류 :(");
       },
     });
   });
-  $("section#login_modal").css("display", "block");
-
-  //$("span#login_span").click(function(){
-  // $("section#login_modal").css("display","none");
-  //})
 });
--- CISUM_nodejs/public/javascripts/login.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-  $("#login_span").click(function () {
-    $("section#login_modal").css("display", "none");
-  });
-});
