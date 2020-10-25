54b34e7a61c684cdb9eb0b5ed9a6ede2a9705ce0
lovelyhyeony <dmlgus1222@naver.com>
2020-10-25 23:56:33 +0900

    1025

--- CISUM_nodejs/public/javascripts/cisumList.js
+++ CISUM_nodejs/public/javascripts/cisumList.js
@@ -38,7 +38,39 @@ $(document).ready(function () {
                 },
             });
         });
-    $(".container-serchList")
-
     
+    $("i.fas.fa-bars").click(function(e){
+        // e.preventDefault();
+        $(".search-list").toggle();
+        $(".container-searchList").toggleClass("open");
+
+        if($(".search-list").hasClass("open")){
+            $(".container-searchList").find("i").attr("class","fas fa-bars");
+        } else {
+            $(".container-searchList").find("i").attr("class","fas fa-search");
+        }
+    })
 });
+/*
+     //접기/펼치기
+        $(".btn").click(function(e){
+            e.preventDefault();
+            $(".nav").slideToggle();
+            $(".btn").toggleClass("open");
+
+            if($(".btn").hasClass("open")){
+                //open이 있을 때
+                $(".btn").find("i").attr("class","fa fa-angle-up");
+            } else {
+                //open이 없을 때
+                $(".btn").find("i").attr("class","fa fa-angle-down");
+            }
+        });
+
+        $(window).resize(function(){
+            var wWidth = $(window).width();
+            if(wWidth > 600){
+                $(".nav").removeAttr("style");
+            }
+        });
+*/
